cc0ead92d8d6d5cf35e9308eaf89caf73dbaefe6
Curtis Autery <ceautery@gmail.com>
2020-03-09 14:55:07 -0400

    Refactor 60 to use test-unit

--- 060-prime_pair_sets.rb
+++ 060-prime_pair_sets.rb
@@ -1,3 +1,17 @@
+# https://projecteuler.net/problem=60
+#
+# The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating them in any order the result will always be prime. For example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
+
+# Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
+
+require 'test-unit'
+
+class Euler060Tests < Test::Unit::TestCase
+  def test_060
+    assert(find_sets(4).include? [3, 7, 109, 673])
+    assert_equal(min_set_total(4), 792)
+  end
+end
 require 'prime'
 
 def rank n
@@ -14,29 +28,33 @@ end
 
 def find_next(sets)
   sets.map do |set|
-    last = set.pop
-    @groups[last]
-      &.select { |n| set.all? { |e| binclude(@groups[e], n) } }
-      &.map { |n| set + [last, n] }
+    last = set[-1]
+    groups[last]
+      &.select { |n| set[0..-2].all? { |e| binclude(groups[e], n) } }
+      &.map { |n| set + [n] }
   end.flatten(1).compact
 end
 
-puts "Generating primes"
-primes = Prime.each(10_000).to_a - [2, 5]
+def primes
+  @primes ||= Prime.each(10_000).to_a - [2, 5]
+end
 
-puts "Finding pairs"
-pairs = primes.combination(2).select { |a, b| concat(a, b).prime? && concat(b, a).prime? }
+def pairs
+  @pairs ||= primes.combination(2).select { |a, b| concat(a, b).prime? && concat(b, a).prime? }
+end
 
-puts "Grouping pairs"
-@groups = pairs.group_by(&:first).transform_values { |group| group.map(&:last) }
+def groups
+  @groups ||= pairs.group_by(&:first).transform_values { |group| group.map(&:last) }
+end
 
-puts "Finding trios"
-trios = find_next(pairs)
+def find_sets size
+  sets = pairs.dup
+  (size - 2).times { sets = find_next(sets) }
+  sets
+end
 
-puts "Finding quads"
-quads = find_next(trios)
+def min_set_total size
+  find_sets(size).map(&:sum).min
+end
 
-puts "Finding pents"
-pents = find_next(quads)
-
-puts pents.map(&:sum).min
+puts min_set_total 5
